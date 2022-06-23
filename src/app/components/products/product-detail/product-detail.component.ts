9dba387a1a2b7919e1e93be04998cb4150913d36
TareqMonwer <tareqmonwer137@gmail.com>
2022-06-24 04:21:31 +0600

    :zap: dynamic product detail page.

--- src/app/components/products/product-detail/product-detail.component.ts
+++ src/app/components/products/product-detail/product-detail.component.ts
@@ -1,6 +1,9 @@
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'ecom-product-detail',
@@ -9,11 +12,14 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ProductDetailComponent implements OnInit {
   productId: number;
+  product$: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private productService: ProductsService) { }
 
   ngOnInit(): void {
     const productId = this.route.snapshot.paramMap.get('id');
     this.productId = Number(productId);
+  
+    this.product$ = this.productService.getProduct(this.productId)
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,17 +1,42 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { Subject, combineLatest, Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  productsUrl: string = environment.apiUrl + '/shop/products/'
   constructor(private http: HttpClient) { }
 
-  products$ = this.http.get(this.productsUrl)
+  productsUrl: string = environment.apiUrl + '/shop/products/'
+  productSelectSubject = new Subject<number>();
+  productSelectAction$ = this.productSelectSubject.asObservable();
+
+  products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
       tap(products => console.log('Products:', products))
-    )
+  )
+
+  // selectedProduct$ = combineLatest([
+  //   this.products$,
+  //   this.productSelectAction$
+  // ]).pipe(
+  //   tap(product => console.log(product, '--before take...')),
+  //   map(([products, productId]) => 
+  //     products.find(product => product.id === 1)
+  //   ),
+  //   tap(product => console.log(product, '--after take...'))
+  // )
+
+  getProduct(productId: number) : Observable<Product> {
+    return this.products$.pipe(
+      map(products => {
+        return products.find(prod => prod.id === productId)
+      }),
+      tap(prod => console.log('product detail:', prod)),
+    );
+  }
 }
