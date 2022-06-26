b1090948de64683c47ac5ce0022511b6da6b840e
TareqMonwer <tareqmonwer137@gmail.com>
2022-06-26 13:08:52 +0600

    :zap: test ProductDetailComponent

--- src/app/components/products/product-detail/product-detail.component.spec.ts
+++ src/app/components/products/product-detail/product-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProductDetailComponent } from './product-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProductsService } from 'src/app/services/products.service';
 
 describe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
@@ -8,6 +10,11 @@ describe('ProductDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [ProductsService],
       declarations: [ ProductDetailComponent ]
     })
     .compileComponents();
