8e9b842df8ac36149f813d9a65c2285bee22f2d2
TareqMonwer <tareqmonwer137@gmail.com>
2022-06-26 13:28:58 +0600

    :zap: test product-card.component.

--- src/app/components/shared/product-card/product-card.component.spec.ts
+++ src/app/components/shared/product-card/product-card.component.spec.ts
@@ -1,3 +1,4 @@
+import { ShortenProductNamePipe } from 'src/app/pipes/shorten-product-name.pipe';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductCardComponent } from './product-card.component';
@@ -8,18 +9,38 @@ describe('ProductCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductCardComponent ]
+      declarations: [
+        ProductCardComponent,
+        ShortenProductNamePipe
+      ],
     })
     .compileComponents();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
+    component.product = {
+      "id": 2,
+      "name": "Apple Watch",
+      "image": "http://localhost:8000/media/products/2022/06/14/apple-watch.jpg",
+      "price": 900.00,
+      "categoryId": 1,
+      "category": "gadget"
+    }
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have right product name', () => {
+    expect(component.product.name)
+      .toEqual("Apple Watch")
+  })
 });
