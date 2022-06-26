8c1e307eaddc2c96b9e306616e7492095728b550
TareqMonwer <tareqmonwer137@gmail.com>
2022-06-26 15:07:07 +0600

    :zap: test login.component, account & product services.

--- src/app/components/accounts/login/login.component.spec.ts
+++ src/app/components/accounts/login/login.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AccountService } from 'src/app/services/account.service';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +11,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        FormsModule
+      ],
+      providers: [AccountService]
     })
     .compileComponents();
   });
--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProductsService);
   });
 
